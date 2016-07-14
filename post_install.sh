4ca876be9cfa1b8419d91a15de6f41c3df735332
Ariel Anders <aanders@mit.edu>
2016-07-14 13:33:50 -0400

    config file for iso and starting script

---
+++ post_install.sh
@@ -0,0 +1,4 @@
+h -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-get update
+
