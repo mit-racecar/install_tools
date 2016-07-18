3204edfec1696ad6c2abcf4422f010a63f1ce58b
Ariel Anders <aanders@mit.edu>
2016-07-18 13:22:16 -0400

    comment

--- post_install.sh
+++ post_install.sh
@@ -19,6 +19,8 @@ git clone https://github.com/mit-racecar/racecar.git
 git clone https://github.com/mit-racecar/vesc.git
 
 
+### INSTALL GAZEBO
+
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
@@ -27,6 +29,8 @@ sudo apt-get --force-yes update
 
 sudo apt-get --force-yes install ros-indigo-controller-manager ros-indigo-gazebo-ros-control ros-indigo-gazebo-ros-pkgs ros-indigo-joint-state-controller ros-indigo-effort-controllers 
 
+
+### SET UP WORKSPACE
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 . ~/.bashrc
 
