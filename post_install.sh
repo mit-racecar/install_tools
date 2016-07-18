7ffef0be81cca3db36dc9e178382db58cbd84176
Ariel Anders <aanders@mit.edu>
2016-07-18 12:12:44 -0400

    force

--- post_install.sh
+++ post_install.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 echo 'installing ros'
-sudo apt-get update -y
+sudo apt-get update --force-yes
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
 
-sudo apt-get -y update
-sudo apt-get -y install libgl1-mesa-dev-lts-utopic vim tmux git
-sudo apt-get -y install ros-indigo-desktop-full
-sudo apt-get -y install ros-indigo-ackermann-msgs
+sudo apt-get --force-yes update
+sudo apt-get --force-yes install libgl1-mesa-dev-lts-utopic vim tmux git
+sudo apt-get --force-yes install ros-indigo-desktop-full
+sudo apt-get --force-yes install ros-indigo-ackermann-msgs
 
-sudo apt-get -y update
+sudo apt-get --force-yes update
 
 mkdir -p ~/racecar-ws/src
 cd ~/racecar-ws/src
@@ -23,9 +24,9 @@ sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `ls
 
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 
-sudo apt-get -y update
+sudo apt-get --force-yes update
 
-sudo apt-get -y install ros-indigo-controller-manager ros-indigo-gazebo-ros-control ros-indigo-gazebo-ros-pkgs ros-indigo-joint-state-controller ros-indigo-effort-controllers
+sudo apt-get --force-yes install ros-indigo-controller-manager ros-indigo-gazebo-ros-control ros-indigo-gazebo-ros-pkgs ros-indigo-joint-state-controller ros-indigo-effort-controllers
 
 
 
