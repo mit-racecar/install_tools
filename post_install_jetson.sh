6da7c97f03f2a074d1bdc25e0fcda0f7c6f52574
Winter Guerra <winterg@mit.edu>
2016-12-01 16:20:39 -0500

    Prompt the user for a new password on post install

--- post_install_jetson.sh
+++ post_install_jetson.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # This file runs post install scripts for the TX1 board.
+echo "Running post install for MIT RACECAR"
 
 # Replace all occurances of 192.168.0.22 in the ~/.bashrc with the interface local IP
 sed -i 's/192.168.0.22/`hostname -I`/g' ~/.bashrc
+
+# Prompt the user for a new password
+echo "Please change the default password for user ubuntu"
+echo "The default password is: ubuntu"
+passwd ubuntu
