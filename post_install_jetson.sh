2125f4544a5ae6a8ac7d3f17fd2ca07000800bd3
Winter Guerra <winterg@mit.edu>
2016-12-01 16:41:37 -0500

    Update post_install_jetson.sh to use wstool update

--- post_install_jetson.sh
+++ post_install_jetson.sh
@@ -4,7 +4,16 @@
 echo "Running post install for MIT RACECAR"
 
 # Replace all occurances of 192.168.0.22 in the ~/.bashrc with the interface local IP
+echo "Updating ~/.bashrc"
 sed -i 's/192.168.0.22/`hostname -I`/g' ~/.bashrc
+
+# Make sure that the rosinstall is up to date
+echo "Updating ~/racecar-ws/.rosinstall"
+wget -q https://raw.githubusercontent.com/mit-racecar/racecar/master/racecar.rosinstall -O ~/racecar-ws/src/.rosinstall
+
+# Update workspace
+echo "Updating ~/racecar-ws/src from github"
+cd ~/racecar-ws/src && wstool update
 
 # Prompt the user for a new password
 echo "Please change the default password for user ubuntu"
